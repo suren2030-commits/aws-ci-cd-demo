c139e2e929de6cc7277bc5093cff10d96175ef62
Suren <surensam2030@gmail.com>
2025-12-05 22:19:12 +0530

    Allow external hosts for Django API

--- backend/airport_kpi/settings.py
+++ backend/airport_kpi/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-7y#%vy4xlm+ok$ol+ruxbp9r1cthppx41p=23k6kuw%wdgnol%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*",]
 
 # Application definition
 
